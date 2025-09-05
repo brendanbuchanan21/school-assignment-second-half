51af253723859f1d917b788a59fd508f51226e44
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-09-04 20:56:09 -0700

    add functionality to part 7 of project

--- IT3240_Wk07_Rev2/js/volunteers.js
+++ IT3240_Wk07_Rev2/js/volunteers.js
@@ -9,12 +9,23 @@ let volunteerArray = [];
 
 const displayVolunteers = function () {   
     // Use findById() to get the HTML element for the display of the volunteer list
+    // Building a numbered list of volunteers 
+    let volunteerlist = "";
 
     // Declare and initialize counter variable to 1 (value for number before name)
+    //declaring variable counter to keep track of numbering
+    let counter = 1;
 
     /* loop over the array of names and print them out as a numbered list in this format
      	1. Jones, Anne
         2. Smith, Jim  */
+    for (let i = 0; i < volunteerArray.length; i++) {
+        volunteerlist += counter + ". " + volunteerArray[i] + "\n";
+        counter++;
+    }
+    findById("volunteer-list-area").value = volunteerlist;
+
+    
 
 	
 };
@@ -38,11 +49,15 @@ const addVolunteer = function () {
 
 const deleteVolunteer = function () {
     // get the name data from the form fields (hint: use the same format as from the add function).
-
+    let volunteerString = findById("last-name-input").value + ", " + findById("first-name-input").value;
     
     // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array).
-	
-   
+	for (let i = 0; i < volunteerArray.length; i++) {
+        if (volunteerArray[i] === volunteerString) {
+            volunteerArray.splice(i, 1);
+            break; // Exiting the loop after deleting the first matching entry
+        }
+    }
 	 
     // display the volunteers and clear the add form
     displayVolunteers();
@@ -55,10 +70,10 @@ const deleteVolunteer = function () {
 
 const clearVolunteers = function () {
     // delete the data from the arrays
-    
+    volunteerArray = [];
         
     // redisplay the empty list
-    
+    displayVolunteers();
 
     // set focus on the first_name input
     findById("first-name-input").focus(); // fixed 08/20/2024
@@ -66,9 +81,9 @@ const clearVolunteers = function () {
 
 const sortVolunteers = function () {
     // sort the volunteers by name
-    
+    volunteerArray.sort();
     // display the sorted names
-
+    displayVolunteers();
 };
 
 // When the page is fully loaded, the buttons will be mapped to the JavaScript functions
